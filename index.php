11c4856ad09b0e6e893713db2eaf146d40ac6906
Nom <mail@mail.fr>
2024-04-08 19:05:36 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Bonjour ! Je m'appelle Souleymane SANOGO</title>
+        <meta name="description" content="App CV">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div class="resume-wrapper">
+            <section class="profile section-padding">
+                <div class="container">
+                    <div class="picture-resume-wrapper">
+                <div class="picture-resume">
+                <span><img src="https://gravatar.com/avatar/aafb48375ac9bb8bf5499f8101c87c2e?size=256&cache=1712238415829" alt="" /></span>
+                <svg version="1.1" viewBox="0 0 350 350">
+
+        <defs>
+            <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -9" result="cm" />
+            </filter>
+        </defs>
+
+
+        <g filter="url(#goo)" >  
+
+        <circle id="main_circle" class="st0" cx="171.5" cy="175.6" r="130"/>
+
+        <circle id="circle" class="bubble0 st1" cx="171.5" cy="175.6" r="122.7"/>
+      
+        <circle id="circle" class="bubble3 st1" cx="171.5" cy="175.6" r="122.7"/>
+        <circle id="circle" class="bubble4 st1" cx="171.5" cy="175.6" r="122.7"/>
+        <circle id="circle" class="bubble5 st1" cx="171.5" cy="175.6" r="122.7"/>
+        <circle id="circle" class="bubble6 st1" cx="171.5" cy="175.6" r="122.7"/>
+        </g>  
+        </svg>
+            </div>
+                <div class="clearfix"></div>
+        </div>
+            <div class="name-wrapper">
+                <h1>SANOGO <br/>Souleymane</h1><!-- YOUR NAME AND LAST NAME  -->
+            </div>
+            <div class="clearfix"></div>
+            <div class="contact-info clearfix">
+                <ul class="list-titles">
+                    <li>Call</li>
+                    <li>Mail</li>
+                    <li>Linkdine</li>
+                    <li>Home</li>
+                </ul>
+                <ul class="list-content ">
+                    <li>07 53 87 08 79</li> <!-- MY PHONE NUMBER  -->
+                    <li>sanogo.s@laposte.net</li> <!-- MY EMAIL -->
+                    <li><a href="https://www.linkedin.com/in/souleymane-sanogo-7a0b3214b/">linkedin.com/in/souleymane-sanogo-7a0b3214b/</a></li> <!-- YOUR WEBSITE  -->
+                    <li>Montpellier, France</li> <!-- MY STATE AND COUNTRY  -->
+                </ul>
+            </div>
+            <div class="contact-presentation"> <!-- MY PRESENTATION RESUME  -->
+                <p>Je suis étudiant en Développer web Full Stack JS; Passionné par le métier de Développer web et web mobile.</p>
+            </div>
+            <div class="contact-social clearfix">
+                <ul class="list-titles">
+                    <li>Linkdine</li>                  
+
+                </ul>
+                <ul class="list-content"> <!-- REMEMBER TO PUT THE URL ON THE HREF TAG  -->
+                    <li><a href="">Email</a></li> <!-- MY EMAIL CONTACT  -->                    
+                </ul>
+            </div>
+            <br/>
+            <div class="contact-social clearfix"><h2>Education</h2></div>
+            <div class="contact-social clearfix">
+                <ul class="list-titles">
+                    <li>Bachelor Accounting</li>
+                    <li>Master 2 Sociology, Social interactions and digital cultures: Imaginaries and Practices</li>
+                    </ul>
+                <ul class="list-content"> <!-- REMEMBER TO PUT THE URL ON THE HREF TAG  -->
+                    <li><a href="">BAMAKO - MALI</a></li>
+                    <br/>
+                    <li><a href="">Université Paul Valéry MONTPELLIER III</a></li>
+
+                </ul>
+            </div>
+                </div>
+            </section>
+
+        <section class="experience section-padding">
+            <div class="container">
+                <h3 class="experience-title">Experience</h3>
+
+                <div class="experience-wrapper">
+                    <div class="company-wrapper clearfix">
+                        <div class="experience-title"> Association UFOLEP 34, Montpellier</div> <!-- NAME OF THE COMPANY -->
+                        <div class="time">01/02/ – 31/03/2022</div> <!-- THE TIME MY WORK WITH THE COMPANY  -->
+                    </div>
+
+                    <div class="job-wrapper clearfix">
+                        <div class="experience-title"> Animateur périscolaire</div> <!-- JOB TITLE  -->
+                        <div class="company-description">
+                            <p>
+                                L'animateur(trice) périscolaire met en oeuvre des animations (activités ludiques, artistiques, manuelles, éducatives et collectives) et des démarches pédagogiques adaptées au cadre de l'accueil périscolaire (temps court avant et après l'école), aux espaces disponibles et au rythme des enfants.,
+                            </p>  <!-- JOB DESCRIPTION  -->
+                        </div>
+                    </div>
+
+                    <div class="company-wrapper clearfix">
+                        <div class="experience-title">Association DEFI, MONTPELLIER</div> <!-- NAME OF THE COMPANY YOUWORK WITH  -->
+                        <div class="time">Novembre 2020 - Février 2021</div> <!-- THE TIME MY WORK WITH THE COMPANY  -->
+                    </div>
+
+                    <div class="job-wrapper clearfix">
+                        <div class="experience-title">Accueil et accompagnement Social</div> <!-- JOB TITLE  -->
+                    <div class="company-description">
+                        <p>DeFI (Développement Formation et Insertion) est une association loi 1901 de droit privé, à but non lucratif, créée en 1995. Elle s’est implantée sur le quartier de la Paillade/ Mosson à Montpellier, reconnu quartier prioritaire de la politique de la ville, ses écoles et colleges sont en Zone REP (éducation prioritaire).;
+                            Son objectif général est de faciliter et de développer la mise en projet d’actions en direction de différents publics en difficultés en proposant :
+                            – De la médiation : un accueil physique ou téléphonique est à la disposition des habitants du quartier, basé sur l’information, l’écoute et l’aide aux démarches administratives.,
+                            – Des formations : des remises à niveau, des ateliers et des formations socio-linguistiques personnalisés pour renforcer les savoirs de base, l’accès aux droits et à l’information.,
+                            – De l’insertion socio-professionnelle : un accompagnement personnalisé pour les allocataires du RSA orientés par le conseil départemental vers la reconversion professionnelle, la formation ou l’emploi.,
+                            – De l’animation : des ateliers collectifs sur la santé, le sport, la culture… Pour sensibiliser, apprendre, découvrir  mais aussi  créer du lien social et favoriser la  participation a la vie sociale.
+                        </p>  <!-- JOB DESCRIPTION  -->
+                    </div>
+                    </div>
+                    <div class="job-wrapper clearfix">
+                        <div class="experience-title">Job d'été</div> <!-- JOB TITLE  -->
+                        <div class="time">01/06/ – 3/09/2022</div> <!-- THE TIME MY WORK WITH THE COMPANY  -->
+                    </div>
+                    
+                    <div class="company-description">
+                     <p>Serveur Restaurant rapid;
+                         
+                    Prendre les commandes et les transmettre au personnel des cuisines et du bar. Servir les boissons et les aliments commandés. Préparer et servir les spécialités de la maison aux tables. Tenir l'inventaire des vins et des verres à vin et en commander au besoin.,</p>  <!-- JOB DESCRIPTION  -->                       
+                    </div>                                 
+            </div><!--Skill experience-->
+
+            <div class="section-wrapper clearfix">
+                <h3 class="section-title">Skills</h3>  <!-- MY SET OF SKILLS  -->
+                    <ul>
+                        <li class="skill-percentage">HTML</li>
+                        <li class="skill-percentage"> BASIC CSS / BOOTSTRAP</li>                       
+                        <li class="skill-percentage">BASIC PHP</li>
+                        <li class="skill-percentage">BASIC MYSQL</li>                        
+                        <li class="skill-percentage">GIT/GITHUB</li>
+
+                    </ul>  
+            </div>
+
+            <div class="section-wrapper clearfix">
+                <h3 class="section-title">Sport-Lecture-Voyage</h3>  <!-- DESCRIPTION OF MY HOBBIES -->     
+            </div>
+
+            </div>
+        </section>
+
+        <div class="clearfix"></div>
+        </div>
+        <script src="" async defer></script>
+    </body>
+</html>
